134ca2a07aeaae42ff6fde39608efa7e66d69cb4
Erin Dubuc <erin.dubuc9@gmail.com>
2018-10-30 17:26:58 +0000

    profile routes working

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
-// initialize express app
-const app = express();
-// middleware = bodyParser
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
+const app = express();
 
 // if data is coming in urlencoded - parse it with json
 app.use(bodyParser.urlencoded({ extended: false }));
 // send back as json so we can make it a Javascript object
 app.use(bodyParser.json());
+
 // can use . notation to require only the mongoURI
 const db = require('./config/keys').mongoURI;
 
@@ -16,12 +16,16 @@ mongoose
     .connect(db)
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err))
+    
+// API routes
+const users = require('./routes/api/users');
+const profiles = require('./routes/api/profiles');
 
+app.use('/api/users', users);
+app.use('/api/profiles', profiles);
 
 app.get('/', (req, res) => res.send('Hello'))
-
 
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log('Listening on port ', port))
-
